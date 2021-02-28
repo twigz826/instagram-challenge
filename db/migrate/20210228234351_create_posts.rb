070e08c84c14c3354334122a0c4320100f7b2bab
twigz826 <thomastwigden@gmail.com>
2021-02-28 20:48:16 -0300

    Ran db:migrate to create a table for posts

--- db/migrate/20210228234351_create_posts.rb
+++ db/migrate/20210228234351_create_posts.rb
@@ -1,7 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.string :image
+      #Checks if post is active or if it has been deleted
+      t.boolean :active
+      #Reference account of user that made that post
+      t.references :user
       t.timestamps
     end
   end
